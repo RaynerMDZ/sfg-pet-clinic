96e22d7bd020e40f79b202cc3497698791810abd
Rayner Mendez <raynermendezg@gmail.com>
2019-03-13 19:41:24 -0400

    Added Speciality Map Implementation. closes #31

---
+++ pet-clinic-data/src/main/java/com/raynermdz/sfgpetclinic/services/SpecialityService.java
@@ -0,0 +1,6 @@
+package com.raynermdz.sfgpetclinic.services;
+
+import com.raynermdz.sfgpetclinic.model.Speciality;
+
+public interface SpecialityService extends CrudService<Speciality, Long> {
+}
---
+++ pet-clinic-data/src/main/java/com/raynermdz/sfgpetclinic/services/map/SpecialityServiceMapImpl.java
@@ -0,0 +1,36 @@
+package com.raynermdz.sfgpetclinic.services.map;
+
+import com.raynermdz.sfgpetclinic.model.Speciality;
+import com.raynermdz.sfgpetclinic.services.SpecialityService;
+import org.springframework.stereotype.Service;
+
+import java.util.Set;
+
+@Service
+public class SpecialityServiceMapImpl extends AbstractMapService<Speciality, Long> implements SpecialityService {
+
+  @Override
+  public Set<Speciality> findAll() {
+    return super.findAll();
+  }
+
+  @Override
+  public Speciality findById(Long id) {
+    return super.findById(id);
+  }
+
+  @Override
+  public Speciality save(Speciality obj) {
+    return super.save(obj);
+  }
+
+  @Override
+  public void delete(Speciality obj) {
+    super.delete(obj);
+  }
+
+  @Override
+  public void deleteById(Long id) {
+    super.deleteById(id);
+  }
+}
