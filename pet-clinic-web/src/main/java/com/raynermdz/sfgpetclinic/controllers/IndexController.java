be2799cf517d63aa856e2c006a40f5657acdf1de
Rayner Mendez <raynermendezg@gmail.com>
2019-03-13 21:58:38 -0400

    Fixed broken links. closes #45

--- pet-clinic-web/src/main/java/com/raynermdz/sfgpetclinic/controllers/IndexController.java
+++ pet-clinic-web/src/main/java/com/raynermdz/sfgpetclinic/controllers/IndexController.java
@@ -15,4 +15,9 @@ public class IndexController {
     model.addAttribute("index", index);
     return "index";
   }
+
+  @RequestMapping({"/oups"})
+  public String oups() {
+    return "notImplemented";
+  }
 }
