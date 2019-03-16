d6171cad6833d8df24e60c77a7b1b1a58dabd227
Rayner Mendez <raynermendezg@gmail.com>
2019-03-16 02:37:41 -0400

    Updated Owner with JPA entities. Closes #36

--- pet-clinic-data/src/main/java/com/raynermdz/sfgpetclinic/model/Owner.java
+++ pet-clinic-data/src/main/java/com/raynermdz/sfgpetclinic/model/Owner.java
@@ -1,13 +1,27 @@
 package com.raynermdz.sfgpetclinic.model;
 
+import org.springframework.data.annotation.TypeAlias;
+
+import javax.persistence.*;
 import java.util.HashSet;
 import java.util.Set;
 
+@Entity
+@Table(name = "owners")
 public class Owner extends Person {
 
+  @Column(name = "address")
   private String address;
+
+  @Column(name = "city")
   private String city;
+
+  @Column(name = "telephone")
   private String telephone;
+
+  // if the owner of a pet is deleted, then the pet will be deleted
+  // as well.
+  @OneToMany(cascade = CascadeType.ALL, mappedBy = "owner")
   private Set<Pet> pets = new HashSet<>();
 
   public String getAddress() {
