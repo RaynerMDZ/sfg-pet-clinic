12ea0f603be9bee0e0c73b33e3fd575b1b9bb440
Rayner Mendez <raynermendezg@gmail.com>
2019-03-16 04:06:55 -0400

    Implemented Map service for Visits. closes #46

---
+++ pet-clinic-data/src/main/java/com/raynermdz/sfgpetclinic/services/map/VisitServiceMapImpl.java
@@ -0,0 +1,41 @@
+package com.raynermdz.sfgpetclinic.services.map;
+
+import com.raynermdz.sfgpetclinic.model.Visit;
+import com.raynermdz.sfgpetclinic.services.springdatajpa.VisitService;
+import org.springframework.stereotype.Service;
+
+import java.util.Set;
+
+@Service
+public class VisitServiceMapImpl extends AbstractMapService<Visit, Long> implements VisitService {
+
+  @Override
+  public Set<Visit> findAll() {
+    return super.findAll();
+  }
+
+  @Override
+  public Visit findById(Long id) {
+    return super.findById(id);
+  }
+
+  @Override
+  public Visit save(Visit visit) {
+
+    if (visit.getPet() == null || visit.getPet().getOwner() == null || visit.getPet().getId() == null
+            || visit.getPet().getOwner().getId() == null) {
+      throw new RuntimeException("Invalid Visit");
+    }
+    return super.save(visit);
+  }
+
+  @Override
+  public void delete(Visit obj) {
+    super.delete(obj);
+  }
+
+  @Override
+  public void deleteById(Long id) {
+    super.deleteById(id);
+  }
+}
---
+++ pet-clinic-data/src/main/java/com/raynermdz/sfgpetclinic/services/springdatajpa/VisitService.java
@@ -0,0 +1,7 @@
+package com.raynermdz.sfgpetclinic.services.springdatajpa;
+
+import com.raynermdz.sfgpetclinic.model.Visit;
+import com.raynermdz.sfgpetclinic.services.CrudService;
+
+public interface VisitService extends CrudService<Visit, Long> {
+}
